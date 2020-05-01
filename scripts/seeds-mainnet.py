aef8f9dd08ae8081fdc69a2ee2f25269c23b64a3
SwingbyDev <dev@dri.network>
2020-05-01 14:04:23 +0200

    Add seeds helper scripts

---
+++ scripts/seeds-mainnet.py
@@ -0,0 +1,6 @@
+import requests, json
+d = requests.get('https://dex-european.binance.org/api/v1/peers').text # replace dex.binance.org with testnet-dex.binance.org for testnet
+l = json.loads(d)
+seeds = ",".join([ (seed["id"]+"@"+seed["original_listen_addr"]) for seed in l if seed["accelerated"] == False])
+print (seeds)
+
