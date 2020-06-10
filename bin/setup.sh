5739dc1c6f89ea348008ca773423c6104144819b
syrohei <syrohei@gmail.com>
2020-06-10 16:04:30 +0800

    Fix setup.sh

--- bin/setup.sh
+++ bin/setup.sh
@@ -3,13 +3,18 @@
 set -ex
 
 if [ ! -d "${BNCHOME}/config/" ]; then
-mkdir -p ${BNCHOME}/config/
-#cp /node-binary/fullnode/${BNET}/${BVER}/config/* ${BNCHOME}/config/
-chown -R bnbchaind:bnbchaind ${BNCHOME}/config/
+    mkdir -p ${BNCHOME}/config/
+    #cp /node-binary/fullnode/${BNET}/${BVER}/config/* ${BNCHOME}/config/
+    chown -R bnbchaind:bnbchaind ${BNCHOME}/config/
 fi
 
-ln -sf /node-binary/fullnode/${BNET}/${BVER}/linux/bnbchaind /usr/local/bin/bnbchaind
-chmod +x /usr/local/bin/bnbchaind
+if [ ${BNET} == "testnet" ]; then
+    ln -sf /node-binary/fullnode/${BNET}/${BVERTEST}/linux/bnbchaind /usr/local/bin/bnbchaind
+    chmod +x /usr/local/bin/bnbchaind
+else
+    ln -sf /node-binary/fullnode/${BNET}/${BVER}/linux/bnbchaind /usr/local/bin/bnbchaind
+    chmod +x /usr/local/bin/bnbchaind
+fi
 
 if [ ${BNET} == "testnet" ]; then
     ln -sf /node-binary/cli/${BNET}/${BVER}/linux/tbnbcli /usr/local/bin/tbnbcli
